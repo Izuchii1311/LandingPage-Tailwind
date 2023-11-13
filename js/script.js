fe4acfd7395368aa46cf30aeb293582561e5ec5b
Izuchii1311 <luthfiramadhan.lr55@gmail.com>
2023-11-13 17:25:28 +0700

    Finish

--- js/script.js
+++ js/script.js
@@ -18,3 +18,28 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+
+// Testimonials
+const mainTesti = document.querySelectorAll('.testimonial-inner');
+// Set elemen pertama sebagai aktif secara default
+mainTesti[1].classList.add('active');
+
+// Jalankan Fungsi ketika di klik
+function showTesti(clickImgTestimoni) {
+    // Hapus kelas 'active' dari elemen sebelumnya
+    mainTesti.forEach(item => {
+        item.classList.remove('active');
+    });
+
+    // Tambahkan kelas 'active' ke elemen yang diklik
+    clickImgTestimoni.classList.add('active');
+
+    // Sembunyikan semua .testimonial-items
+    document.querySelectorAll('.testimonial-items').forEach(item => {
+        item.classList.add('hidden');
+    });
+
+    // Tampilkan .testimonial-items yang terkait dengan elemen yang diklik
+    clickImgTestimoni.querySelector('.testimonial-items').classList.remove('hidden');
+}
